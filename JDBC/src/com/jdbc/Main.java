7293d5e424a0eb4a172e0fb6a460cb281282fcab
puneet verma <puneetverma359@gmail.com>
2023-01-08 21:40:56 +0530

    Update Main.java

--- JDBC/src/com/jdbc/Main.java
+++ JDBC/src/com/jdbc/Main.java
@@ -5,21 +5,30 @@ import java.sql.*;
 public class Main {
 
 	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
 		
-		String url="jdbc:mysql://localhost:3306/demo?verifyServerCertificate=false&useSSL=true";
-		String uname="root";
-		String pass="puneet@12345";
-	  
-		String query="select * from student";
+		String query="select * from student where id=2";
+		String quer1="insert into student (name,id) values('robin','65');";
 		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con=DriverManager.getConnection(url,uname,pass);
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		//Connection con=DriverManager.getConnection(url,uname,pass);
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","demo","newuser@123");  
 		
 		  Statement stam=con.createStatement();
 		  ResultSet res=stam.executeQuery(query);
-		  res.next(); String val=res.getString("name"); System.out.println(val);
-		 
+			/*
+			 * while(res.next()) {
+			 * 
+			 * String val=res.getString("name"); String val1=res.getString("id");
+			 * System.out.println(val+ " "+val1); }
+			 */
+		  res.next();
+			String vald=res.getString(1);
+			String vald1=res.getString(2);
+			System.out.println(vald +" "+ vald1);
+			stam.close();
+			res.close();
+			System.out.println("---------------------");
+			//System.out.println(res.getInt(1));
 			
 	}
 
